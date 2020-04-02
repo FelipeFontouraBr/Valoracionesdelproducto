d3212f9f450cb776a718440b9d77fdcec0e299c7
Felipe Fontoura <felipefontouramec@gmail.com>
2020-04-02 19:14:50 +0200

    Creado archivo HTML, Css y JavaScript

---
+++ script.js
@@ -0,0 +1,35 @@
+  var vCont1 = 0;
+  function addlike1(){
+    vCont1 = vCont1 + 1;
+    document.getElementById('but1').value = vCont1;
+  }
+  var vCont2 = 0;
+  function addlike2(){
+    vCont2 = vCont2 + 1;
+    document.getElementById('but2').value = vCont2;
+  }
+  var vCont3 = 0;
+  function addlike3(){
+    vCont3 = vCont3 + 1;
+    document.getElementById('but3').value = vCont3;
+  }
+  var vCont4 = 0;
+  function addlike4(){
+    vCont4 = vCont4 + 1;
+    document.getElementById('but4').value = vCont4;
+  }
+  var vCont5 = 0;
+  function addlike5(){
+    vCont5 = vCont5 + 1;
+    document.getElementById('but5').value = vCont5;
+  }
+  var vCont6 = 0;
+  function addlike6(){
+    vCont6 = vCont6 + 1;
+    document.getElementById('but6').value = vCont6;
+  }
+  var vCont7 = 0;
+  function addlike7(){
+    vCont7 = vCont7 + 1;
+    document.getElementById('but7').value = vCont7;
+  }  
